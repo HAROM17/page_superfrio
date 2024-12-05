b87a95ece74cc1f4154a3601b2cce8baa586006c
Hans <135499209+HAROM17@users.noreply.github.com>
2024-12-05 10:52:06 -0500

    4

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const accountLink = document.getElementById("accountLinkMovil");
-
-    accountLink.addEventListener("click", function (event) {
-        event.preventDefault();
-        
-        // Mostrar el modal de inicio de sesión
-        const loginModal = new bootstrap.Modal(document.getElementById("authModal"));
-        loginModal.show();
-    });
-});
 
 
 
---
+++ assets/js/login_movil.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const accountLink = document.getElementById("accountLinkMovil");
+
+    accountLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        
+        // Mostrar el modal de inicio de sesión
+        const loginModal = new bootstrap.Modal(document.getElementById("authModal"));
+        loginModal.show();
+    });
+});
